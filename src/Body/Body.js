9ffcd07b1cdd1d8ffa2e2ca8c77ecae4b24e92f0
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-05 10:40:58 +0530

    Basic Setup Created

---
+++ src/Body/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./body.css"
+
+const Body = () => {
+    return (
+        <React.Fragment>
+            <div className="body">
+                Birth Day Reminder App
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Body;
